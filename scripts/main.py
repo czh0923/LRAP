c20d9ea8fb2e319da228c3dcdf757e5f97619564
czh0923 <zchenggn@126.com>
2022-02-27 13:51:09 -0800

    paragraphs and unrecognizable char

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,7 @@ punctuation2 = [',']
 '''
 (1) some words. -> some words.
 [1] some words. -> some words.
+source: https://stackoverflow.com/questions/14596884/remove-text-between-and
 '''
 def delete_parenthesis(sentence) -> str:
     return re.sub("[\(\[].*?[\)\]]", "", sentence)
@@ -13,6 +14,7 @@ def delete_parenthesis(sentence) -> str:
 
 '''
 dots ... word, end. -> ['dots', '...', 'word', ',', 'end', '.']
+source: https://www.geeksforgeeks.org/python-split-string-on-all-punctuations/
 '''
 def match(sentence) -> list:
     return re.findall( r'\w+|[^\s\w]+', sentence)
@@ -39,8 +41,18 @@ def add_spaces(list_of_words) -> list:
     return result
 
 
+def manage_paragraph():
+    # TODO
+    pass
 
-def write_word_to_file(sentence, w_file):
+'''
+format: 0 or 1. 0 is putting each sentence on one line. 1 is regular paragraph format. 
+
+return: list of paragraphs that are recognized
+'''
+def write_word_to_file(sentence, w_file, format=0):
+
+    paragraphs = []
 
     without_parenthesis = delete_parenthesis(sentence)
 
@@ -50,18 +62,39 @@ def write_word_to_file(sentence, w_file):
 
     for word in list_of_words_with_space:
 
-        w_file.write(word)
-        
-        if word in punctuation1:
+        try:
+            w_file.write(word)
+        except:
+            print("unrecognizable\r")
+
+        if word.isdigit():
             w_file.write('\r')
+            w_file.write('\r')
+            paragraphs.append(word)
+            manage_paragraph()
+        
+
+        if format == 0:
+            char = '\r'
+        elif format == 1:
+            char = ' '
+
+        if word in punctuation1:
+            w_file.write(char)
+    
+    return paragraphs
        
+def main():
+    f = open('longer_test.txt','r', encoding='UTF-8')
+    contents = f.read()
+    #contents = contents.decode('UTF-8')
 
-f = open('testing.txt','r', encoding='UTF-8')
-contents = f.read()
-#contents = contents.decode('UTF-8')
+    f2 = open('longer_test_result.txt', 'w')
+    found_paragraphs = write_word_to_file(contents, f2)
 
-f2 = open('testing_result.txt', 'w')
-write_word_to_file(contents, f2)
+    print(found_paragraphs)
 
-f.close()
-f2.close()
+    f.close()
+    f2.close()
+
+main()
