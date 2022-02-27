fe96d89b2ea9634630b1c9843799c96c44de1f34
czh0923 <zchenggn@126.com>
2022-02-27 13:15:28 -0800

    v2, ignoring paragraph number

--- scripts/format_sentence.py
+++ scripts/format_sentence.py
@@ -1,31 +1,67 @@
 import re
 
+punctuation1 = ['.', '?', '!']
+punctuation2 = [',']
+
+'''
+(1) some words. -> some words.
+[1] some words. -> some words.
+'''
 def delete_parenthesis(sentence) -> str:
     return re.sub("[\(\[].*?[\)\]]", "", sentence)
 
-def format_sentence(sentence, file):
 
-    #without_parenthesis = delete_parenthesis(sentence)
-
-    sentenceSplit = filter(None, sentence.split(". "))
-    for s in sentenceSplit :
-        line = s.strip() + "."
-        print(line)
-        #line = line.decode('UTF-8')
-        try:
-            file.write(line + '\r')
-        except:
-            pass
+'''
+dots ... word, end. -> ['dots', '...', 'word', ',', 'end', '.']
+'''
+def match(sentence) -> list:
+    return re.findall( r'\w+|[^\s\w]+', sentence)
 
 
+'''
+['dots', '...', 'word', ',', 'end', '.'] -> ['dots', ' ', '...', ' ', 'word', ',', ' ', 'end', '.']
+'''
+def add_spaces(list_of_words) -> list:
+    result = []
 
+    for word in list_of_words:
+        if word not in punctuation1 and word not in punctuation2:
+            result.append(word)
+            result.append(' ')
+        elif word in punctuation2:
+            result.pop()
+            result.append(word)
+            result.append(' ')
+        else:
+            result.pop()
+            result.append(word)
+
+    return result
+
+
+
+def write_word_to_file(sentence, w_file):
+
+    without_parenthesis = delete_parenthesis(sentence)
+
+    list_of_words = match(without_parenthesis)
+
+    list_of_words_with_space = add_spaces(list_of_words)
+
+    for word in list_of_words_with_space:
+
+        w_file.write(word)
+        
+        if word in punctuation1:
+            w_file.write('\r')
+       
 
 f = open('testing.txt','r', encoding='UTF-8')
 contents = f.read()
 #contents = contents.decode('UTF-8')
 
 f2 = open('testing_result.txt', 'w')
-format_sentence(contents, f2)
+write_word_to_file(contents, f2)
 
 f.close()
 f2.close()
